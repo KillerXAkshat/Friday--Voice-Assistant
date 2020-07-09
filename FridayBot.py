e386dcb300f0e5f247cd447eb48b0f12715ec7cd
KillerXAkshat <akshatgurnani@outlook.com>
2020-07-09 13:28:36 +0530

    Added Spotify Module and updated the working tree

--- FridayBot.py
+++ FridayBot.py
@@ -15,13 +15,11 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
     engine.setProperty("rate",145)
     
-
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour >=0 and hour<12:
@@ -33,10 +31,8 @@ def wishMe():
 
     speak("I am Friday. Please tell me how may I help you!")
     
-
 def takeCommand():
     # It takes microphone input from the user and returns string output
-    
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -47,7 +43,6 @@ def takeCommand():
         print("Recognizing...")
         query = r.recognize_google(audio, language= 'en-in')
         
-
     except Exception as e:
         print(e)
         engine.setProperty("rate", 120)
@@ -64,11 +59,9 @@ def sendEmail(to, content):
     server.sendmail('your_email_address', to, content)
     server.close()
 
-
 if __name__ == "__main__":
     wishMe()
     while True:
-
         query = takeCommand().lower()
 
          # Logic for executing tasks based on query
@@ -95,39 +88,31 @@ if __name__ == "__main__":
         elif 'open googlecolab' in query:
             webbrowser.open("www.googlecolab.com")
 
+        elif 'spotify' in query:
+            os.startfile('C:\\Program Files\\Spotify\\Spotify.exe')     
+
         elif 'open music player' in query:
          speak("Opening Music Player")
          root = Tk()
          root.minsize(300,300)
-
          listofsongs = []
          realnames = []
-
          v = StringVar()
          songlabel = Label(root,textvariable=v,width=35)
-
          index = 0
 
          def directorychooser():
-
              directory = askdirectory()
              os.chdir(directory)
-
              for files in os.listdir(directory):
                  if files.endswith(".mp3"):
-
                      realdir = os.path.realpath(files)
                      audio = ID3(realdir)
                      realnames.append(audio['TIT2'].text[0])
-
-
                      listofsongs.append(files)
-
-
              pygame.mixer.init()
              pygame.mixer.music.load(listofsongs[0])
              #pygame.mixer.music.play()
-
          directorychooser()
 
          def updatelabel():
@@ -161,7 +146,6 @@ if __name__ == "__main__":
              v.set("")
              root.destroy()
 
-
          label = Label(root,text='Music Player')
          label.pack()
 
@@ -193,7 +177,6 @@ if __name__ == "__main__":
          previousbutton.bind("<Button-1>",prevsong)
          stopbutton.bind("<Button-1>",stopsong)
          quitbutton.bind("<Button-1>",quitplayer)
-
          songlabel.pack()
          root.mainloop()
     
